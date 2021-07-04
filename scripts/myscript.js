d0fc5a26ac92ff712ebf8a245a0767f06d7a96d1
Amritha Susan Jacob <amrithasusanjacob@gmail.com>
2021-07-04 14:20:45 -0400

    Added Navigation Overlay

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -12,3 +12,13 @@ $(document).ready(function() {
     
     beeRight();
 });
+
+/* Open */
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+  }
+  
+  /* Close */
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+  }
